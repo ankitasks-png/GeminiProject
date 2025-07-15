6d33adc767b72e0ce56d9761bc668d0f3ec2d26e
Ankita Satish Kumar <ankitasatish0711@gmail.com>
2025-07-15 21:29:01 +0530

    testing_gemini.py

--- testing_gemini.py
+++ testing_gemini.py
@@ -1,14 +1,13 @@
-import google.generativeai as genai  # ✅ This line was missing
+import google.generativeai as genai  
 
-# Enter your  API key
+#   API key
 genai.configure(api_key="AIzaSyBK5yWIQotaB1G7a23yPQrAYQhyeMCZiaQ")
 
 
 model = genai.GenerativeModel(model_name="models/chat-bison-001")
 
-# Enter sample prompt
+#  sample prompt
 response = model.generate_content("Enter a prompt")
 
-# Output the result
-print("✅ Gemini says:")
+# Output 
 print(response.text)
